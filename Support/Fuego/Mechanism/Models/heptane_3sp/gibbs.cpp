e6ad1839d77b194e09ab44ff850c9489652e5d81
Marco Arienti <marient@sandia.gov>
2019-10-09 15:02:51 -0400

    compatible now

---
+++ Support/Fuego/Mechanism/Models/heptane_3sp/gibbs.cpp
@@ -0,0 +1,486 @@
+
+/*compute the g/(RT) at the given temperature */
+/*tc contains precomputed powers of T, tc[0] = log(T) */
+void gibbs(double * restrict species, double * restrict tc)
+{
+
+    /*temperature */
+    double T = tc[1];
+    double invT = 1 / T;
+
+    /*species with midpoint at T=1000 kelvin */
+    if (T < 1000) {
+        /*species 1: O2 */
+        species[1] =
+            -1.005249020000000e+03 * invT
+            -2.821801190000000e+00
+            -3.212936400000000e+00 * tc[0]
+            -5.637431750000000e-04 * tc[1]
+            +9.593584116666666e-08 * tc[2]
+            -1.094897691666667e-10 * tc[3]
+            +4.384276960000000e-14 * tc[4];
+        /*species 2: N2 */
+        species[2] =
+            -1.020900000000000e+03 * invT
+            -6.516950000000001e-01
+            -3.298677000000000e+00 * tc[0]
+            -7.041200000000000e-04 * tc[1]
+            +6.605369999999999e-07 * tc[2]
+            -4.701262500000001e-10 * tc[3]
+            +1.222427500000000e-13 * tc[4];
+    } else {
+        /*species 1: O2 */
+        species[1] =
+            -1.233930180000000e+03 * invT
+            +5.084126000000002e-01
+            -3.697578190000000e+00 * tc[0]
+            -3.067598445000000e-04 * tc[1]
+            +2.098069983333333e-08 * tc[2]
+            -1.479401233333333e-12 * tc[3]
+            +5.682176550000000e-17 * tc[4];
+        /*species 2: N2 */
+        species[2] =
+            -9.227977000000000e+02 * invT
+            -3.053888000000000e+00
+            -2.926640000000000e+00 * tc[0]
+            -7.439885000000000e-04 * tc[1]
+            +9.474601666666666e-08 * tc[2]
+            -8.414199999999999e-12 * tc[3]
+            +3.376675500000000e-16 * tc[4];
+    }
+
+    /*species with midpoint at T=1391 kelvin */
+    if (T < 1391) {
+        /*species 0: NC7H16 */
+        species[0] =
+            -2.565865650000000e+04 * invT
+            -3.664165307000000e+01
+            +1.268361870000000e+00 * tc[0]
+            -4.271779100000000e-02 * tc[1]
+            +8.755779766666667e-06 * tc[2]
+            -1.357881008333333e-09 * tc[3]
+            +1.011974625000000e-13 * tc[4];
+    } else {
+        /*species 0: NC7H16 */
+        species[0] =
+            -3.427600810000000e+04 * invT
+            +1.145189165000000e+02
+            -2.221489690000000e+01 * tc[0]
+            -1.738378750000000e-02 * tc[1]
+            +1.973452150000000e-06 * tc[2]
+            -1.527487316666667e-10 * tc[3]
+            +5.306513300000000e-15 * tc[4];
+    }
+    return;
+}
+
+
+/*compute the a/(RT) at the given temperature */
+/*tc contains precomputed powers of T, tc[0] = log(T) */
+void helmholtz(double * restrict species, double * restrict tc)
+{
+
+    /*temperature */
+    double T = tc[1];
+    double invT = 1 / T;
+
+    /*species with midpoint at T=1000 kelvin */
+    if (T < 1000) {
+        /*species 1: O2 */
+        species[1] =
+            -1.00524902e+03 * invT
+            -3.82180119e+00
+            -3.21293640e+00 * tc[0]
+            -5.63743175e-04 * tc[1]
+            +9.59358412e-08 * tc[2]
+            -1.09489769e-10 * tc[3]
+            +4.38427696e-14 * tc[4];
+        /*species 2: N2 */
+        species[2] =
+            -1.02090000e+03 * invT
+            -1.65169500e+00
+            -3.29867700e+00 * tc[0]
+            -7.04120000e-04 * tc[1]
+            +6.60537000e-07 * tc[2]
+            -4.70126250e-10 * tc[3]
+            +1.22242750e-13 * tc[4];
+    } else {
+        /*species 1: O2 */
+        species[1] =
+            -1.23393018e+03 * invT
+            -4.91587400e-01
+            -3.69757819e+00 * tc[0]
+            -3.06759845e-04 * tc[1]
+            +2.09806998e-08 * tc[2]
+            -1.47940123e-12 * tc[3]
+            +5.68217655e-17 * tc[4];
+        /*species 2: N2 */
+        species[2] =
+            -9.22797700e+02 * invT
+            -4.05388800e+00
+            -2.92664000e+00 * tc[0]
+            -7.43988500e-04 * tc[1]
+            +9.47460167e-08 * tc[2]
+            -8.41420000e-12 * tc[3]
+            +3.37667550e-16 * tc[4];
+    }
+
+    /*species with midpoint at T=1391 kelvin */
+    if (T < 1391) {
+        /*species 0: NC7H16 */
+        species[0] =
+            -2.56586565e+04 * invT
+            -3.76416531e+01
+            +1.26836187e+00 * tc[0]
+            -4.27177910e-02 * tc[1]
+            +8.75577977e-06 * tc[2]
+            -1.35788101e-09 * tc[3]
+            +1.01197462e-13 * tc[4];
+    } else {
+        /*species 0: NC7H16 */
+        species[0] =
+            -3.42760081e+04 * invT
+            +1.13518917e+02
+            -2.22148969e+01 * tc[0]
+            -1.73837875e-02 * tc[1]
+            +1.97345215e-06 * tc[2]
+            -1.52748732e-10 * tc[3]
+            +5.30651330e-15 * tc[4];
+    }
+    return;
+}
+
+
+/*compute Cv/R at the given temperature */
+/*tc contains precomputed powers of T, tc[0] = log(T) */
+void cv_R(double * restrict species, double * restrict tc)
+{
+
+    /*temperature */
+    double T = tc[1];
+
+    /*species with midpoint at T=1000 kelvin */
+    if (T < 1000) {
+        /*species 1: O2 */
+        species[1] =
+            +2.21293640e+00
+            +1.12748635e-03 * tc[1]
+            -5.75615047e-07 * tc[2]
+            +1.31387723e-09 * tc[3]
+            -8.76855392e-13 * tc[4];
+        /*species 2: N2 */
+        species[2] =
+            +2.29867700e+00
+            +1.40824000e-03 * tc[1]
+            -3.96322200e-06 * tc[2]
+            +5.64151500e-09 * tc[3]
+            -2.44485500e-12 * tc[4];
+    } else {
+        /*species 1: O2 */
+        species[1] =
+            +2.69757819e+00
+            +6.13519689e-04 * tc[1]
+            -1.25884199e-07 * tc[2]
+            +1.77528148e-11 * tc[3]
+            -1.13643531e-15 * tc[4];
+        /*species 2: N2 */
+        species[2] =
+            +1.92664000e+00
+            +1.48797700e-03 * tc[1]
+            -5.68476100e-07 * tc[2]
+            +1.00970400e-10 * tc[3]
+            -6.75335100e-15 * tc[4];
+    }
+
+    /*species with midpoint at T=1391 kelvin */
+    if (T < 1391) {
+        /*species 0: NC7H16 */
+        species[0] =
+            -2.26836187e+00
+            +8.54355820e-02 * tc[1]
+            -5.25346786e-05 * tc[2]
+            +1.62945721e-08 * tc[3]
+            -2.02394925e-12 * tc[4];
+    } else {
+        /*species 0: NC7H16 */
+        species[0] =
+            +2.12148969e+01
+            +3.47675750e-02 * tc[1]
+            -1.18407129e-05 * tc[2]
+            +1.83298478e-09 * tc[3]
+            -1.06130266e-13 * tc[4];
+    }
+    return;
+}
+
+
+/*compute Cp/R at the given temperature */
+/*tc contains precomputed powers of T, tc[0] = log(T) */
+void cp_R(double * restrict species, double * restrict tc)
+{
+
+    /*temperature */
+    double T = tc[1];
+
+    /*species with midpoint at T=1000 kelvin */
+    if (T < 1000) {
+        /*species 1: O2 */
+        species[1] =
+            +3.21293640e+00
+            +1.12748635e-03 * tc[1]
+            -5.75615047e-07 * tc[2]
+            +1.31387723e-09 * tc[3]
+            -8.76855392e-13 * tc[4];
+        /*species 2: N2 */
+        species[2] =
+            +3.29867700e+00
+            +1.40824000e-03 * tc[1]
+            -3.96322200e-06 * tc[2]
+            +5.64151500e-09 * tc[3]
+            -2.44485500e-12 * tc[4];
+    } else {
+        /*species 1: O2 */
+        species[1] =
+            +3.69757819e+00
+            +6.13519689e-04 * tc[1]
+            -1.25884199e-07 * tc[2]
+            +1.77528148e-11 * tc[3]
+            -1.13643531e-15 * tc[4];
+        /*species 2: N2 */
+        species[2] =
+            +2.92664000e+00
+            +1.48797700e-03 * tc[1]
+            -5.68476100e-07 * tc[2]
+            +1.00970400e-10 * tc[3]
+            -6.75335100e-15 * tc[4];
+    }
+
+    /*species with midpoint at T=1391 kelvin */
+    if (T < 1391) {
+        /*species 0: NC7H16 */
+        species[0] =
+            -1.26836187e+00
+            +8.54355820e-02 * tc[1]
+            -5.25346786e-05 * tc[2]
+            +1.62945721e-08 * tc[3]
+            -2.02394925e-12 * tc[4];
+    } else {
+        /*species 0: NC7H16 */
+        species[0] =
+            +2.22148969e+01
+            +3.47675750e-02 * tc[1]
+            -1.18407129e-05 * tc[2]
+            +1.83298478e-09 * tc[3]
+            -1.06130266e-13 * tc[4];
+    }
+    return;
+}
+
+
+/*compute the e/(RT) at the given temperature */
+/*tc contains precomputed powers of T, tc[0] = log(T) */
+void speciesInternalEnergy(double * restrict species, double * restrict tc)
+{
+
+    /*temperature */
+    double T = tc[1];
+    double invT = 1 / T;
+
+    /*species with midpoint at T=1000 kelvin */
+    if (T < 1000) {
+        /*species 1: O2 */
+        species[1] =
+            +2.21293640e+00
+            +5.63743175e-04 * tc[1]
+            -1.91871682e-07 * tc[2]
+            +3.28469308e-10 * tc[3]
+            -1.75371078e-13 * tc[4]
+            -1.00524902e+03 * invT;
+        /*species 2: N2 */
+        species[2] =
+            +2.29867700e+00
+            +7.04120000e-04 * tc[1]
+            -1.32107400e-06 * tc[2]
+            +1.41037875e-09 * tc[3]
+            -4.88971000e-13 * tc[4]
+            -1.02090000e+03 * invT;
+    } else {
+        /*species 1: O2 */
+        species[1] =
+            +2.69757819e+00
+            +3.06759845e-04 * tc[1]
+            -4.19613997e-08 * tc[2]
+            +4.43820370e-12 * tc[3]
+            -2.27287062e-16 * tc[4]
+            -1.23393018e+03 * invT;
+        /*species 2: N2 */
+        species[2] =
+            +1.92664000e+00
+            +7.43988500e-04 * tc[1]
+            -1.89492033e-07 * tc[2]
+            +2.52426000e-11 * tc[3]
+            -1.35067020e-15 * tc[4]
+            -9.22797700e+02 * invT;
+    }
+
+    /*species with midpoint at T=1391 kelvin */
+    if (T < 1391) {
+        /*species 0: NC7H16 */
+        species[0] =
+            -2.26836187e+00
+            +4.27177910e-02 * tc[1]
+            -1.75115595e-05 * tc[2]
+            +4.07364302e-09 * tc[3]
+            -4.04789850e-13 * tc[4]
+            -2.56586565e+04 * invT;
+    } else {
+        /*species 0: NC7H16 */
+        species[0] =
+            +2.12148969e+01
+            +1.73837875e-02 * tc[1]
+            -3.94690430e-06 * tc[2]
+            +4.58246195e-10 * tc[3]
+            -2.12260532e-14 * tc[4]
+            -3.42760081e+04 * invT;
+    }
+    return;
+}
+
+
+/*compute the h/(RT) at the given temperature (Eq 20) */
+/*tc contains precomputed powers of T, tc[0] = log(T) */
+void speciesEnthalpy(double * restrict species, double * restrict tc)
+{
+
+    /*temperature */
+    double T = tc[1];
+    double invT = 1 / T;
+
+    /*species with midpoint at T=1000 kelvin */
+    if (T < 1000) {
+        /*species 1: O2 */
+        species[1] =
+            +3.21293640e+00
+            +5.63743175e-04 * tc[1]
+            -1.91871682e-07 * tc[2]
+            +3.28469308e-10 * tc[3]
+            -1.75371078e-13 * tc[4]
+            -1.00524902e+03 * invT;
+        /*species 2: N2 */
+        species[2] =
+            +3.29867700e+00
+            +7.04120000e-04 * tc[1]
+            -1.32107400e-06 * tc[2]
+            +1.41037875e-09 * tc[3]
+            -4.88971000e-13 * tc[4]
+            -1.02090000e+03 * invT;
+    } else {
+        /*species 1: O2 */
+        species[1] =
+            +3.69757819e+00
+            +3.06759845e-04 * tc[1]
+            -4.19613997e-08 * tc[2]
+            +4.43820370e-12 * tc[3]
+            -2.27287062e-16 * tc[4]
+            -1.23393018e+03 * invT;
+        /*species 2: N2 */
+        species[2] =
+            +2.92664000e+00
+            +7.43988500e-04 * tc[1]
+            -1.89492033e-07 * tc[2]
+            +2.52426000e-11 * tc[3]
+            -1.35067020e-15 * tc[4]
+            -9.22797700e+02 * invT;
+    }
+
+    /*species with midpoint at T=1391 kelvin */
+    if (T < 1391) {
+        /*species 0: NC7H16 */
+        species[0] =
+            -1.26836187e+00
+            +4.27177910e-02 * tc[1]
+            -1.75115595e-05 * tc[2]
+            +4.07364302e-09 * tc[3]
+            -4.04789850e-13 * tc[4]
+            -2.56586565e+04 * invT;
+    } else {
+        /*species 0: NC7H16 */
+        species[0] =
+            +2.22148969e+01
+            +1.73837875e-02 * tc[1]
+            -3.94690430e-06 * tc[2]
+            +4.58246195e-10 * tc[3]
+            -2.12260532e-14 * tc[4]
+            -3.42760081e+04 * invT;
+    }
+    return;
+}
+
+
+/*compute the S/R at the given temperature (Eq 21) */
+/*tc contains precomputed powers of T, tc[0] = log(T) */
+void speciesEntropy(double * restrict species, double * restrict tc)
+{
+
+    /*temperature */
+    double T = tc[1];
+
+    /*species with midpoint at T=1000 kelvin */
+    if (T < 1000) {
+        /*species 1: O2 */
+        species[1] =
+            +3.21293640e+00 * tc[0]
+            +1.12748635e-03 * tc[1]
+            -2.87807523e-07 * tc[2]
+            +4.37959077e-10 * tc[3]
+            -2.19213848e-13 * tc[4]
+            +6.03473759e+00 ;
+        /*species 2: N2 */
+        species[2] =
+            +3.29867700e+00 * tc[0]
+            +1.40824000e-03 * tc[1]
+            -1.98161100e-06 * tc[2]
+            +1.88050500e-09 * tc[3]
+            -6.11213750e-13 * tc[4]
+            +3.95037200e+00 ;
+    } else {
+        /*species 1: O2 */
+        species[1] =
+            +3.69757819e+00 * tc[0]
+            +6.13519689e-04 * tc[1]
+            -6.29420995e-08 * tc[2]
+            +5.91760493e-12 * tc[3]
+            -2.84108828e-16 * tc[4]
+            +3.18916559e+00 ;
+        /*species 2: N2 */
+        species[2] =
+            +2.92664000e+00 * tc[0]
+            +1.48797700e-03 * tc[1]
+            -2.84238050e-07 * tc[2]
+            +3.36568000e-11 * tc[3]
+            -1.68833775e-15 * tc[4]
+            +5.98052800e+00 ;
+    }
+
+    /*species with midpoint at T=1391 kelvin */
+    if (T < 1391) {
+        /*species 0: NC7H16 */
+        species[0] =
+            -1.26836187e+00 * tc[0]
+            +8.54355820e-02 * tc[1]
+            -2.62673393e-05 * tc[2]
+            +5.43152403e-09 * tc[3]
+            -5.05987313e-13 * tc[4]
+            +3.53732912e+01 ;
+    } else {
+        /*species 0: NC7H16 */
+        species[0] =
+            +2.22148969e+01 * tc[0]
+            +3.47675750e-02 * tc[1]
+            -5.92035645e-06 * tc[2]
+            +6.10994927e-10 * tc[3]
+            -2.65325665e-14 * tc[4]
+            -9.23040196e+01 ;
+    }
+    return;
+}
